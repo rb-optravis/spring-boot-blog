44488d6f94662a236bddf79a9bb9123d1939d181
Rafael Biehler <rafael.biehler@optravis.com>
2026-01-09 11:34:00 +0100

    Add kotest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	kotlin("jvm") version "2.2.21"
 	kotlin("plugin.spring") version "2.2.21"
+	id("io.kotest") version "6.0.7"
 	id("org.springframework.boot") version "4.0.1"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -8,6 +9,8 @@ plugins {
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 description = "Demo project for Spring Boot"
+
+val kotestVersion = "6.0.7"
 
 java {
 	toolchain {
@@ -33,6 +36,12 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("com.ninja-squad:springmockk:5.0.1")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// kotest
+	testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+	testImplementation("io.kotest:kotest-property:$kotestVersion")
+	testImplementation("io.kotest:kotest-framework-engine:${kotestVersion}")
+	testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
 }
 
 kotlin {
